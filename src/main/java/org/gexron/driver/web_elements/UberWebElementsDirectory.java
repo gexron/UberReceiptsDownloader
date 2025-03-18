734af0985f63671b2ba8d0e9cfcc89f858e187c8
Jorge <gexron91@gmail.com>
2025-03-18 19:12:16 +0200

    removed unused code

--- src/main/java/org/gexron/driver/web_elements/UberWebElementsDirectory.java
+++ src/main/java/org/gexron/driver/web_elements/UberWebElementsDirectory.java
@@ -13,20 +13,12 @@ import java.util.List;
 public class UberWebElementsDirectory implements WebElementsDirectory {
     private static UberWebElementsDirectory uberWebElementsDirectory;
 
-    private static final String PHONE_EMAIL_ID = "PHONE_NUMBER_or_EMAIL_ADDRESS";
-    private static final String OTP_XPATH = "//input[contains(@id, 'OTP')]";
-    private static final String PASSWORD_ID = "PASSWORD";
-    private static final String TRIPS_PERIOD_LIST_XPATH = "//div/div[@data-baseweb='select'][2]/div";
     private static final String TRIPS_SECTION_HEADER_XPATH = "//h3";
     private static final String MORE_BUTTON_XPATH = "//button[@data-baseweb='button']";
-    private static final String CURRENT_MONTH_OPTION_XPATH = "//ul[@data-baseweb='menu']/li[3]";//TODO: return value back to 3
     private static final String FIRST_LOAD_LAST_TRIP_DIV_XPATH = "//main/div/div[1]/div[3]/div";
     private static final String FIRST_LOAD_TRIPS_DIVS_XPATH = "//main/div/div[1]/div[4]/span/div";
     private static final String MORE_LOADED_TRIPS_DIVS_XPATH = "//main/div/div/div[2]/span/div";
-    private static final String VIEW_RECEIPT_BUTTON_XPATH = "//div[@data-tracking-name='cta-button-row']/div/button";
-    private static final String DOWNLOAD_PDF_LINK_XPATH = "//a[@data-tracking-name='download-receipt-pdf-link']";
-    private static final String TITLE_XPATH = "//h1";
-    private static final String FORWARD_BUTTON_ID = "forward-button";
+
 
     // First load
     ////main/div/div[1]/div[3]/div
@@ -43,20 +35,6 @@ public class UberWebElementsDirectory implements WebElementsDirectory {
     private UberWebElementsDirectory() {
         
     }
-    @Override
-    public WebElement getUsernameTextbox(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(PHONE_EMAIL_ID)));
-    }
-
-    @Override
-    public WebElement getPasswordTextbox(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(PASSWORD_ID)));
-    }
-
-    @Override
-    public List<WebElement> getOtpTextbox(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.xpath(OTP_XPATH)));
-    }
 
     @Override
     public WebElement getMoreButton(WebDriver driver) {
@@ -66,16 +44,6 @@ public class UberWebElementsDirectory implements WebElementsDirectory {
     @Override
     public WebElement getTripsSectionHeader(WebDriverWait driverWait) {
         return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(TRIPS_SECTION_HEADER_XPATH)));
-    }
-
-    @Override
-    public WebElement getTripsPeriodListButton(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(TRIPS_PERIOD_LIST_XPATH)));
-    }
-
-    @Override
-    public WebElement getCurrentMonthOption(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(CURRENT_MONTH_OPTION_XPATH)));
     }
 
     @Override
@@ -95,25 +63,6 @@ public class UberWebElementsDirectory implements WebElementsDirectory {
     @Override
     public List<WebElement> getTripsDivsAfterLoadingMoreResults(WebDriver driver) {
         return driver.findElements(By.xpath(MORE_LOADED_TRIPS_DIVS_XPATH));
-    }
-
-    @Override
-    public WebElement getViewReceiptButton(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(VIEW_RECEIPT_BUTTON_XPATH)));
-    }
-
-    @Override
-    public WebElement getDownloadPdfButton(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(DOWNLOAD_PDF_LINK_XPATH)));
-    }
-
-    @Override
-    public WebElement getTitle(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(TITLE_XPATH)));
-    }
-
-    public WebElement getContinueButton(WebDriverWait driverWait) {
-        return driverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(FORWARD_BUTTON_ID)));
     }
 
     public String getClickScript() {
--- src/main/java/org/gexron/driver/web_elements/WebElementsDirectory.java
+++ src/main/java/org/gexron/driver/web_elements/WebElementsDirectory.java
@@ -8,31 +8,13 @@ import java.util.List;
 
 public interface WebElementsDirectory {
 
-    WebElement getUsernameTextbox(WebDriverWait driverWait);
-
-    WebElement getPasswordTextbox(WebDriverWait driverWait);
-
-    List<WebElement> getOtpTextbox(WebDriverWait driverWait);
-
     WebElement getMoreButton(WebDriver driver);
 
     WebElement getTripsSectionHeader(WebDriverWait driverWait);
 
-    WebElement getTripsPeriodListButton(WebDriverWait driverWait);
-
-    WebElement getCurrentMonthOption(WebDriverWait driverWait);
-
     List<WebElement> getTripsDivsAfterInitialLoading(WebDriver driver);
 
     List<WebElement> getTripsDivsAfterLoadingMoreResults(WebDriver driver);
-
-    WebElement getViewReceiptButton(WebDriverWait driverWait);
-
-    WebElement getDownloadPdfButton(WebDriverWait driverWait);
-
-    WebElement getContinueButton(WebDriverWait driverWait);
-
-    WebElement getTitle(WebDriverWait driverWait);
 
     String getClickScript();
 }
